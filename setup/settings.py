a239ad16866e613d810966752cafbd63b725f47f
Wanderson <wheredia2@gmail.com>
2026-08-24 16:55:27 -0300

    Corrige Erro 500: Adiciona HtmxMiddleware e django_htmx ao settings

--- setup/settings.py
+++ setup/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # Terceiros
     "django_htmx",
     # Apps do Projeto
     "core",
